95d3fa8fd13ee2b031134259d9fdcbeb4862aedb
minyov <minyov@yandex.ru>
2017-08-20 21:44:45 -0800

    Reaction to invalid login or password

--- src/components/LoginPage/index.js
+++ src/components/LoginPage/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { TouchableHighlight, View, Text } from 'react-native';
 import { connect } from 'react-redux';
+import PopupDialog, { SlideAnimation } from 'react-native-popup-dialog';
 import { auth } from '../../api';
 import { setUser } from '../../actions/user';
 import TextInputWithHeader from './TextInputWithHeader';
@@ -9,9 +10,13 @@ import styles from './styles';
 class LoginPage extends Component {
   login = (login, password) => {
     auth(login, (user) => {
-      this.props.dispatch(setUser(user));
-      
-      this.props.navigation.navigate('ChatsList');
+      if (user != null) {
+        this.props.dispatch(setUser(user));
+        
+        this.props.navigation.navigate('ChatsList');
+      } else {
+        this.popupRef.show();
+      }
     });
   }
 
@@ -38,6 +43,17 @@ class LoginPage extends Component {
             <Text style={ styles.touchableTextStyle }>Sign In</Text>
           </TouchableHighlight>
         </View>
+
+        <PopupDialog 
+          dialogAnimation = { new SlideAnimation({ slideFrom: 'bottom' }) }
+          ref={ (ref) => this.popupRef = ref}
+          height={ 30 }
+          width={ 200 }
+        >
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}> 
+            <Text>Invalid login or password</Text>
+          </View>
+        </PopupDialog>
       </View>
     );
   }
